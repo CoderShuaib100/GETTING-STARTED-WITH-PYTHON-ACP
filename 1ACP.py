2b7f60d546375807975a2c2a6058aa7538266dcb
CoderShuaib100 <sayeedishuaib@gmail.com>
2024-11-17 13:41:40 +0530

    first commit

---
+++ 1ACP.py
@@ -0,0 +1,14 @@
+# creating the variables
+name = "Muhammed Shuaib Sayeedi S.H."
+grade = 9
+SchoolName = "Bhuvana Krishnan Matriculation Higher Secondary School."
+address = "Kelambakkam,chenagalpattu,TamilNadu,India,Asia,Earth,Solar system,Milky Way,Universe"
+DOB = "13 October 2010"
+
+# printing statements using those variables
+
+print("Hi, My name is",name)
+print("I am studying in grade",grade)
+print("I study in",SchoolName)
+print("I was born on",DOB,"and I am living in",address)
+print("This is my Bio-Data, the address is very accurate using this aliens can also find me without getting lost!")
